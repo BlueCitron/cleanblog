dfe7410b6d769d92e98918d701e8716ab8595a3e
BlueCitron <sds901234@naver.com>
2020-03-24 22:33:52 +0900

    - Logger : logback -> log4j2 교체 - 404 페이지 추가 - 500시 GlobalController에서 처리   - 에러 로그파일에 로깅   - 메인화면으로 리다이렉트

--- src/main/java/bluecitron/cleanblog/web/GlobalControllerAdvice.java
+++ src/main/java/bluecitron/cleanblog/web/GlobalControllerAdvice.java
@@ -6,8 +6,16 @@ import bluecitron.cleanblog.web.service.CategoryService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
+import javax.annotation.PostConstruct;
+import javax.servlet.http.HttpServletRequest;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Arrays;
 import java.util.List;
 
 @Slf4j
@@ -20,5 +28,15 @@ public class GlobalControllerAdvice {
     @ModelAttribute("categories")
     public List<CategoryDto> categories() {
         return categoryService.getList(new CategoryCommand());
+    }
+
+    @ExceptionHandler(Exception.class)
+    public String globalExceptionHandler(HttpServletRequest request, Exception e) {
+
+        log.error("{}", e.getMessage());
+        Arrays.stream(e.getStackTrace())
+                .forEach(stackTraceElement -> log.error(stackTraceElement.toString()));
+
+        return "redirect:/";
     }
 }
